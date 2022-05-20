8824e12eebabbe405a47b1d4edf53534baf533b2
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2022-05-20 15:32:40 -0500

    Does not message to local store upon creation (save use case)

---
+++ KrakenVaultCoreTests/Local Vault Feature/SaveVaultItemsUseCaseTests.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright © 2022 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import KrakenVaultCore
+import XCTest
+
+final class SaveVaultItemsUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageLocalStoreUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.messages, [])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (LocalVaultLoader, VaultStoreSpy) {
+        let store = VaultStoreSpy()
+        let sut = LocalVaultLoader(store: store)
+
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
