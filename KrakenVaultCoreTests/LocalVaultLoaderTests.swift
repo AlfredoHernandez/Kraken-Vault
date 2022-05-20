17dca892ea5fce992c5e7cbe53a71d4e2d5adc6a
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2022-05-20 12:18:11 -0500

    Load requests retrieve items

--- KrakenVaultCoreTests/LocalVaultLoaderTests.swift
+++ KrakenVaultCoreTests/LocalVaultLoaderTests.swift
@@ -8,6 +8,13 @@ import XCTest
 final class LocalVaultLoaderTests: XCTestCase {
     func test_init_doesNotMessageLocalStoreUponCreation() {
         let store = VaultStoreSpy()
+        _ = LocalVaultLoader(store: store)
+
+        XCTAssertEqual(store.messages, [])
+    }
+
+    func test_load_requestsRetrieveItems() {
+        let store = VaultStoreSpy()
         let sut = LocalVaultLoader(store: store)
 
         sut.load()
