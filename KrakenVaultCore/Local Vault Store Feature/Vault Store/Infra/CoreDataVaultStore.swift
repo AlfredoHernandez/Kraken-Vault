23883d22424c865533b38a9e73024f7319f8ef52
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2022-05-22 15:39:42 -0500

    Refactor `CoreDataVaultStore`

--- KrakenVaultCore/Local Vault Store Feature/Vault Store/Infra/CoreDataVaultStore.swift
+++ KrakenVaultCore/Local Vault Store Feature/Vault Store/Infra/CoreDataVaultStore.swift
@@ -5,7 +5,7 @@
 import CoreData
 import Foundation
 
-public final class CoreDataVaultStore: VaultStore {
+public final class CoreDataVaultStore {
     private static let modelName = "KrakenVaultStore"
     private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataVaultStore.self))
 
@@ -30,26 +30,28 @@ public final class CoreDataVaultStore: VaultStore {
         }
     }
 
+    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = context
+        context.perform { action(context) }
+    }
+}
+
+extension CoreDataVaultStore: VaultStore {
     public func retrieve(completion: @escaping (RetrievalResult) -> Void) {
-        do {
-            try context.performAndWait {
-                let vaultItems = try ManagedVaultItem.all(in: context)
-                completion(
-                    .success(vaultItems.map {
-                        VaultStoreItem(uuid: $0.uuid, name: $0.name, username: $0.username, password: $0.password, url: $0.url)
-                    })
-                )
-            }
-        } catch {
-            completion(.failure(error))
+        perform { context in
+            completion(Result {
+                try ManagedVaultItem.all(in: context).map {
+                    VaultStoreItem(uuid: $0.uuid, name: $0.name, username: $0.username, password: $0.password, url: $0.url)
+                }
+            })
         }
     }
 
     public func delete(_: VaultStoreItem, completion _: @escaping (Result<Void, Error>) -> Void) {}
 
     public func insert(_ item: VaultStoreItem, completion: @escaping (Result<Void, Error>) -> Void) {
-        completion(Result {
-            try ManagedVaultItem.create(from: item, in: context)
-        })
+        perform { context in
+            completion(Result { try ManagedVaultItem.create(from: item, in: context) })
+        }
     }
 }
